9f7d59052989bfda72f479872d44a4b232b0d833
Tokyo <pasha0754@gmail.com>
2023-12-20 23:53:29 +0300

    cors update

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const betRouter = require('./routes/bet.routes');
 const instructionRouter = require('./routes/instruction.routes');
 const workerRouter = require('./routes/worker.routes');
 const shiftRouter = require('./routes/shift.routes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Middleware для настройки заголовков CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/api', clientRouter);
 app.use('/api', projectRouter);
 app.use('/api', articleRouter);
@@ -19,5 +29,5 @@ app.use('/api', workerRouter);
 app.use('/api', shiftRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
